1fff2593f4f758d51479c3754c5b7ab7bd33950f
Emanuelle Maranan <marananemanuelle@gmail.com>
2024-12-06 12:19:38 +0800

    style: embedded google map on about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -63,8 +63,17 @@
                 </div>
             @endforeach
         </div>
+    </x-section>
 
-        <x-maps-leaflet :centerPoint="['lat' => 52.16, 'long' => 5]"></x-maps-leaflet>
+    <x-section class="bg-white">
+        <x-slot:icon><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pinned"><path d="M18 8c0 3.613-3.869 7.429-5.393 8.795a1 1 0 0 1-1.214 0C9.87 15.429 6 11.613 6 8a6 6 0 0 1 12 0"/><circle cx="12" cy="8" r="2"/><path d="M8.714 14h-3.71a1 1 0 0 0-.948.683l-2.004 6A1 1 0 0 0 3 22h18a1 1 0 0 0 .948-1.316l-2-6a1 1 0 0 0-.949-.684h-3.712"/></svg></x-slot:icon>
+        <x-slot:heading>Our Location</x-slot:heading>
+        <x-slot:subheading>Waze your way to Amazing View!</x-slot:subheading>
 
+        <x-maps-google
+            :centerPoint="['lat' => 14.442312, 'long' => 121.396931]"
+            :markers="[['lat' => 14.442312, 'long' => 121.396931]]"
+            :zoomLevel="14"
+        ></x-maps-google>
     </x-section>
 </x-guest-layout>
