1af0a1e122f785d85007db9b79a47bd087a76e1f
Emanuelle Maranan <marananemanuelle@gmail.com>
2025-04-10 22:32:04 +0800

    test: testing confirmed email

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -359,7 +359,7 @@ class ReservationService
                 );
             }
 
-            GenerateReservationPDF::dispatch($reservation);
+            // GenerateReservationPDF::dispatch($reservation);
             return $reservation;
         });
     }
