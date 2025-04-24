0b43296073e0a1b612a22512cd5556b71d8e4540
Emanuelle Maranan <marananemanuelle@gmail.com>
2025-04-25 01:19:34 +0800

    fix: confirmation type 'discount' to 'discounts'

--- app/Livewire/App/Reservation/ConfirmReservation.php
+++ app/Livewire/App/Reservation/ConfirmReservation.php
@@ -271,19 +271,19 @@ class ConfirmReservation extends Component
                                         </hgroup>
 
                                         <div>
-                                            @if ($discount->image)
-                                                <x-img src="{{ $discount->image }}" />
+                                            @if ($discounts->image)
+                                                <x-img src="{{ $discounts->image }}" />
                                             @endif
 
                                             <div class="flex items-center justify-between mt-5">
                                                 <div>
-                                                    <p class="text-sm font-semibold">{{ $discount->description }}</p>
-                                                    <p class="text-xs">Amount: <x-currency />{{ number_format($discount->amount, 2) }}</p>
+                                                    <p class="text-sm font-semibold">{{ $discounts->description }}</p>
+                                                    <p class="text-xs">Amount: <x-currency />{{ number_format($discounts->amount, 2) }}</p>
                                                 </div>
 
-                                                @if ($discount->image)
+                                                @if ($discounts->image)
                                                     <x-tooltip text="Download Image" dir="left">
-                                                        <a href="{{ asset('storage/' . $discount->image) }}" download>
+                                                        <a href="{{ asset('storage/' . $discounts->image) }}" download>
                                                             <x-icon-button x-ref="content">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-image-down-icon lucide-image-down"><path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21"/><path d="m14 19 3 3v-5.5"/><path d="m17 22 3-3"/><circle cx="9" cy="9" r="2"/></svg>
                                                             </x-icon-button>
