133ec85e89697424fde7d9993e3b96dc99070314
Emanuelle Maranan <marananemanuelle@gmail.com>
2025-04-25 23:07:41 +0800

    fix: match the server time to local time

--- app/Http/Controllers/DateController.php
+++ app/Http/Controllers/DateController.php
@@ -9,8 +9,11 @@ use Illuminate\Support\Facades\Http;
 class DateController extends Controller
 {
     public static function today() {
-        logger(Carbon::createFromTimestamp($_SERVER['REQUEST_TIME'], env('APP_TIMEZONE'))->format('Y-m-d g:i A'));
-        return Carbon::createFromTimestamp($_SERVER['REQUEST_TIME'], env('APP_TIMEZONE'))->addHours(13)->format('Y-m-d');
+        if (env('APP_ENV') == 'local') {
+            return Carbon::createFromTimestamp($_SERVER['REQUEST_TIME'], env('APP_TIMEZONE'))->format('Y-m-d');
+        }
+
+        return Carbon::createFromTimestamp($_SERVER['REQUEST_TIME'], env('APP_TIMEZONE'))->addHours(8)->format('Y-m-d');
     }
 
     public static function tomorrow() {
@@ -18,6 +21,6 @@ class DateController extends Controller
             return Carbon::createFromTimestamp($_SERVER['REQUEST_TIME'], env('APP_TIMEZONE'))->addDay()->format('Y-m-d');
         }
 
-        return Carbon::createFromTimestamp($_SERVER['REQUEST_TIME'], env('APP_TIMEZONE'))->addHours(39)->format('Y-m-d');
+        return Carbon::createFromTimestamp($_SERVER['REQUEST_TIME'], env('APP_TIMEZONE'))->addHours(8)->addDay()->format('Y-m-d');
     }
 }
