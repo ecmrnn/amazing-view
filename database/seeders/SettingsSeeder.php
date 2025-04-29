7dcd4fb10b1e2b638fe863b30558c086426b7ec9
Emanuelle Maranan <marananemanuelle@gmail.com>
2025-04-30 06:41:33 +0800

    fix: fixed setting seeder

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -14,46 +14,46 @@ class SettingsSeeder extends Seeder
     public function run(): void
     {
         $settings = collect([
-            // [
-            //     'key' => 'site_logo',
-            //     'value' => '',
-            //     'type' => 'image',
-            // ],
-            // [
-            //     'key' => 'site_title',
-            //     'value' => 'Amazing View Mountain Resort',
-            //     'type' => 'text',
-            // ],
-            // [
-            //     'key' => 'site_tagline',
-            //     'value' => 'Experience Serenity and Luxury here in Amazing View!',
-            //     'type' => 'text',
-            // ],
-            // [
-            //     'key' => 'site_phone',
-            //     'value' => '09171399334',
-            //     'type' => 'text',
-            // ],
-            // [
-            //     'key' => 'site_email',
-            //     'value' => 'reservation@amazingviewresort.com',
-            //     'type' => 'text',
-            // ],
-            // [
-            //     'key' => 'site_gcash_phone',
-            //     'value' => '09171399334',
-            //     'type' => 'text',
-            // ],
-            // [
-            //     'key' => 'site_gcash_name',
-            //     'value' => 'Fabio Basbaño',
-            //     'type' => 'text',
-            // ],
-            // [
-            //     'key' => 'site_gcash_qr',
-            //     'value' => '',
-            //     'type' => 'image',
-            // ],
+            [
+                'key' => 'site_logo',
+                'value' => '',
+                'type' => 'image',
+            ],
+            [
+                'key' => 'site_title',
+                'value' => 'Amazing View Mountain Resort',
+                'type' => 'text',
+            ],
+            [
+                'key' => 'site_tagline',
+                'value' => 'Experience Serenity and Luxury here in Amazing View!',
+                'type' => 'text',
+            ],
+            [
+                'key' => 'site_phone',
+                'value' => '09171399334',
+                'type' => 'text',
+            ],
+            [
+                'key' => 'site_email',
+                'value' => 'reservation@amazingviewresort.com',
+                'type' => 'text',
+            ],
+            [
+                'key' => 'site_gcash_phone',
+                'value' => '09171399334',
+                'type' => 'text',
+            ],
+            [
+                'key' => 'site_gcash_name',
+                'value' => 'Fabio Basbaño',
+                'type' => 'text',
+            ],
+            [
+                'key' => 'site_gcash_qr',
+                'value' => '',
+                'type' => 'image',
+            ],
             [
                 'key' => 'site_reservation_downpayment_percentage',
                 'value' => '.5',
